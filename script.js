7ab5fa7321a1c0396e2266c226186b155c175030
LiamShortDev <liamshort.workplace@gmail.com>
2025-07-31 19:04:15 +0100

    Implemented basic bookshelf layout

---
+++ script.js
@@ -0,0 +1,21 @@
+fetch('projects.json')
+  .then(response => response.json())
+  .then(data => {
+    data.forEach(project => {
+      const card = document.createElement('a');
+      card.className = 'project-card';
+      card.href = project.link;
+
+      card.innerHTML = `
+        <img src="${project.image}" alt="${project.title}">
+        <div class="overlay">
+          <h3>${project.title}</h3>
+          <p>${project.description}</p>
+        </div>
+      `;
+
+      const shelf = document.querySelector(`#${project.category} .project-grid`);
+      if (shelf) shelf.appendChild(card);
+    });
+  })
+  .catch(err => console.error("Failed to load projects:", err));
